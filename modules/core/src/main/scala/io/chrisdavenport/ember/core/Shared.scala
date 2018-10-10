572306afad140b637d275b5c19b59f9e99834103
Chris Davenport <chris@christopherdavenport.tech>
2018-10-10 09:56:45 -0400

    Use Custom Errors Where Possible

--- modules/core/src/main/scala/io/chrisdavenport/ember/core/Shared.scala
+++ modules/core/src/main/scala/io/chrisdavenport/ember/core/Shared.scala
@@ -27,8 +27,8 @@ object Shared {
   /** evaluates address from the host port and scheme, if this is a custom scheme we will default to port 8080**/
   def addressForRequest[F[_]: Sync](req: Request[F]): F[InetSocketAddress] = 
     for {
-      scheme <- req.uri.scheme.toRight(new IllegalArgumentException("Missing Scheme")).liftTo[F]
-      host <- req.uri.host.toRight(new IllegalArgumentException("Missing Host")).liftTo[F]
+      scheme <- req.uri.scheme.toRight(EmberException.IncompleteClientRequest("Scheme")).liftTo[F]
+      host <- req.uri.host.toRight(EmberException.IncompleteClientRequest("Host")).liftTo[F]
       socketAddress <- addressForComponents(scheme, host, req.uri.port)
     } yield socketAddress
     
@@ -39,7 +39,7 @@ object Shared {
         case Uri.Scheme.http => 80.some
         case _ => Option.empty[Int]
       }
-    }.toRight(new IllegalArgumentException("Missing Port"))
+    }.toRight(EmberException.IncompleteClientRequest("Port"))
     .liftTo[F]
     .map(new InetSocketAddress(host.value, _))
   }
