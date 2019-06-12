6355d892f2aa397bab95436afddacd593074d690
Chris Davenport <chris@christopherdavenport.tech>
2019-06-12 07:28:12 -0700

    Random Doc Improvements

--- modules/client/src/main/scala/io/chrisdavenport/ember/client/EmberClientBuilder.scala
+++ modules/client/src/main/scala/io/chrisdavenport/ember/client/EmberClientBuilder.scala
@@ -18,7 +18,6 @@ import org.http4s.client._
 
 
 final class EmberClientBuilder[F[_]: Concurrent: Timer: ContextShift] private (
-    // private val sslExecutionContext: ExecutionContext
   private val sslContextOpt : Option[(ExecutionContext, SSLContext)]
   , private val acgR: Resource[F, AsynchronousChannelGroup]
   , val maxTotal: Int
